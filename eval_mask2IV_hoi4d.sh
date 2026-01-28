4da87ca79c4c3e25808c3e67b5f5fe1f747fd09b
jes-bro <jes@ericbrown.net>
2026-01-28 15:49:54 -0800

    Create eval slurm job script and update paths in 2stage hoi4d eval scropt

---
+++ eval_mask2IV_hoi4d.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=simurgh
+#SBATCH --partition=simurgh --qos=normal
+#SBATCH --time=168:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=256G
+
+# only use the following on partition with GPUs
+#SBATCH --gres=gpu:l40s:2
+
+#SBATCH --job-name="hoi4d m2iv inf"
+#SBATCH --output=sample-%j.out
+
+# only use the following if you want email notification
+#SBATCH --mail-user=jesb@stanford.edu
+#SBATCH --mail-type=ALL
+
+# list out some useful information (optional)
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+# sample process (list hostnames of the nodes you've requested)
+source ~/.bashrc
+conda deactivate
+conda activate dyncraft
+sh /simurgh2/projects/Mask2IV/Mask2IV/scripts/inference_hoi4d_2stage.py
+
+# can try the following to list out which GPU you have access to
+#srun /usr/local/cuda/samples/1_Utilities/deviceQuery/deviceQuery
+
+# done
+echo "Done"
--- scripts/run_hoi4d_2stage.sh
+++ scripts/run_hoi4d_2stage.sh
@@ -1,18 +1,18 @@
 # version=$1 ##1024, 512, 256
 seed=42
 
-ckpt1=""
+ckpt1="/simurgh2/projects/Mask2IV/checkpoints/pretrained_mask2iv/hoi4d_model.ckpt"
 config1=configs/inference_512_hoi4d_first.yaml
 
 # maskcat
-ckpt2=""
+ckpt2="/simurgh2/projects/Mask2IV/checkpoints/pretrained_mask2iv/second_stage/hoi4d_model.ckpt"
 config2=configs/inference_512_hoi4d_maskcat.yaml
 
 prompt_dir=prompts/hoi4d
 res_dir="./exp_outputs/Mask2IV-inference/maskcat/hoi4d"
 
 
-CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluation/inference_hoi4d_2stage.py \
+python3 /simurgh2/projects/Mask2IV/Mask2IV/scripts/evaluation/inference_hoi4d_2stage.py \
 --seed ${seed} \
 --ckpt_path1 $ckpt1 \
 --ckpt_path2 $ckpt2 \
